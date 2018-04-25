73678f5491aef75beabe3954683163509484ffbb
Vanderson <vandersonwebdev@gmail.com>
2018-04-25 20:12:54 -0300

    adicionando a pasta do app novamente, renomei pra um teste e esqueci de reverter antes de commitar

---
+++ app/app.py
@@ -0,0 +1,62 @@
+import json
+import requests
+
+from flask import Flask, render_template, abort, request
+
+app = Flask('app')
+
+def get_cities():
+    r_cities = requests.get('http://localhost:8882/cities/')
+    cities = []
+
+    if r_cities.status_code == 200:
+        r_cities_data = json.loads(r_cities.text)
+        for i in r_cities_data:
+            cities.append({'name': i['district'], 'id': i['woeid']})
+        print(cities)
+        return cities
+    else:
+        abort(404)
+
+
+def get_city_by_id(id):
+    r_cities = requests.get('http://localhost:8882/cities/')
+    if r_cities.status_code == 200:
+        r_cities_data = json.loads(r_cities.text)
+        for i in r_cities_data:
+            if i['woeid'] == id:
+                return i['district']
+
+
+
+def get_weather():
+    r_weather = requests.get('http://localhost:8882/weather/')
+    weather = []
+
+    if r_weather.status_code == 200:
+        r_weather_data = json.loads(r_weather.text)
+        for i in r_weather_data:
+            weather.append(i['name'])
+        print(weather)
+        return weather
+    else:
+        abort(404)
+
+
+def get_vacations_days(city, days, weather):
+    pass
+
+@app.route("/")
+def home():
+    cities = get_cities()
+    weather_list = get_weather()
+    return render_template('home.html', cities=cities, weather_list=weather_list)
+
+
+@app.route("/result/", methods=['GET'])
+def result():
+    city = get_city_by_id(request.args.get('cidade'))
+    dias = request.args.get('qtd_dias')
+    list_clima = request.args.getlist('clima')
+    vacation_list = get_vacations_days(city, dias, list_clima)
+    return render_template('result.html', city=city)
