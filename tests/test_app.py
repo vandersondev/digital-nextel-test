0313efc1efb22665130cf257eb213358dab9c63b
Vanderson <vandersonwebdev@gmail.com>
2018-04-25 20:10:23 -0300

    listagem real de cidades e climas na home

---
+++ tests/test_app.py
@@ -0,0 +1,14 @@
+import pytest
+
+
+@pytest.fixture
+def client_app():
+    from myapp.app import app as app
+    app.testing = True
+    app = app.test_client()
+    return app
+
+
+def test_home(client_app):
+    response = client_app.get('/')
+    assert response.status_code == 200
